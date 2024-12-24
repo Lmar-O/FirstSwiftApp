5f44d6940b6f7f330996df96a956c8d32c7f76ef
Lmar-O <115730198+Lmar-O@users.noreply.github.com>
2024-12-24 16:51:51 -0600

    Initial commit

---
+++ Prodo/Prodo/ProdoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProdoApp.swift
+//  Prodo
+//
+//  Created by Lmar Oria on 12/25/24.
+//
+
+import SwiftUI
+
+@main
+struct ProdoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Prodo/ProdoTests/ProdoTests.swift
@@ -0,0 +1,17 @@
+//
+//  ProdoTests.swift
+//  ProdoTests
+//
+//  Created by Lmar Oria on 12/25/24.
+//
+
+import Testing
+@testable import Prodo
+
+struct ProdoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
